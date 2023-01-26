cc1446b107a11ccdfe8281564b90b81cd9cd4339
St1r3S <mikmuael.steam@gmail.com>
2023-01-26 22:22:04 +0200

    minor fixes in data sample

--- src/test/resources/sql/university_data_sample.sql
+++ src/test/resources/sql/university_data_sample.sql
@@ -1,21 +1,7 @@
 INSERT INTO specialisms(specialism_name)
-VALUES ('112');
-INSERT INTO specialisms(specialism_name)
-VALUES ('113');
-INSERT INTO specialisms(specialism_name)
-VALUES ('116');
-INSERT INTO specialisms(specialism_name)
-VALUES ('121');
-INSERT INTO specialisms(specialism_name)
 VALUES ('122');
 INSERT INTO specialisms(specialism_name)
-VALUES ('123');
-INSERT INTO specialisms(specialism_name)
-VALUES ('124');
-INSERT INTO specialisms(specialism_name)
 VALUES ('125');
-INSERT INTO specialisms(specialism_name)
-VALUES ('126');
 INSERT INTO specialisms(specialism_name)
 VALUES ('127');
 
@@ -25,24 +11,6 @@ INSERT INTO schedule_days(day_of_week, semester_type)
 VALUES ('Tuesday', 'fall');
 INSERT INTO schedule_days(day_of_week, semester_type)
 VALUES ('Wednesday', 'fall');
-INSERT INTO schedule_days(day_of_week, semester_type)
-VALUES ('Thursday', 'fall');
-INSERT INTO schedule_days(day_of_week, semester_type)
-VALUES ('Friday', 'fall');
-INSERT INTO schedule_days(day_of_week, semester_type)
-VALUES ('Saturday', 'fall');
-INSERT INTO schedule_days(day_of_week, semester_type)
-VALUES ('Monday', 'spring');
-INSERT INTO schedule_days(day_of_week, semester_type)
-VALUES ('Tuesday', 'spring');
-INSERT INTO schedule_days(day_of_week, semester_type)
-VALUES ('Wednesday', 'spring');
-INSERT INTO schedule_days(day_of_week, semester_type)
-VALUES ('Thursday', 'spring');
-INSERT INTO schedule_days(day_of_week, semester_type)
-VALUES ('Friday', 'spring');
-INSERT INTO schedule_days(day_of_week, semester_type)
-VALUES ('Saturday', 'spring');
 
 INSERT INTO rooms(room_number)
 VALUES ('404');
@@ -50,48 +18,20 @@ INSERT INTO rooms(room_number)
 VALUES ('505');
 INSERT INTO rooms(room_number)
 VALUES ('606');
-INSERT INTO rooms(room_number)
-VALUES ('707');
 
 INSERT INTO academic_years(year_number, semester_type)
 VALUES (1, 'fall');
 INSERT INTO academic_years(year_number, semester_type)
-VALUES (1, 'spring');
-INSERT INTO academic_years(year_number, semester_type)
 VALUES (2, 'fall');
 INSERT INTO academic_years(year_number, semester_type)
-VALUES (2, 'spring');
-INSERT INTO academic_years(year_number, semester_type)
 VALUES (3, 'fall');
-INSERT INTO academic_years(year_number, semester_type)
-VALUES (3, 'spring');
-INSERT INTO academic_years(year_number, semester_type)
-VALUES (4, 'fall');
-INSERT INTO academic_years(year_number, semester_type)
-VALUES (4, 'spring');
-
 
 INSERT INTO groupss(group_name, specialism_id, academic_year_id)
-VALUES ('AI-191', 5, 1);
+VALUES ('AI-193', 1, 1);
 INSERT INTO groupss(group_name, specialism_id, academic_year_id)
-VALUES ('AI-192', 5, 1);
+VALUES ('AI-194', 1, 1);
 INSERT INTO groupss(group_name, specialism_id, academic_year_id)
-VALUES ('AI-193', 5, 1);
-INSERT INTO groupss(group_name, specialism_id, academic_year_id)
-VALUES ('AI-194', 5, 1);
-INSERT INTO groupss(group_name, specialism_id, academic_year_id)
-VALUES ('AI-195', 5, 1);
-INSERT INTO groupss(group_name, specialism_id, academic_year_id)
-VALUES ('AI-181', 5, 3);
-INSERT INTO groupss(group_name, specialism_id, academic_year_id)
-VALUES ('AI-182', 5, 3);
-INSERT INTO groupss(group_name, specialism_id, academic_year_id)
-VALUES ('AI-183', 5, 3);
-INSERT INTO groupss(group_name, specialism_id, academic_year_id)
-VALUES ('AI-184', 5, 3);
-INSERT INTO groupss(group_name, specialism_id, academic_year_id)
-VALUES ('AI-185', 5, 3);
-
+VALUES ('AI-195', 1, 1);
 
 INSERT INTO users(user_type, user_name, password_hash, user_role, first_name, last_name, birthday, email)
 VALUES (1, 'maccas82', 'pass82', 'Admin', 'Michael', 'Maccas', '1982-06-22', 'maccas82@gmail.com');
