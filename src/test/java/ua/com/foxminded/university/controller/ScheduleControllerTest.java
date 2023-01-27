ef4ca1b822ea1a27c362addab2af85b14ea0bb7e
anton <anton.h@openlegacy.com>
2023-01-27 15:59:24 +0200

    test fixes

--- src/test/java/ua/com/foxminded/university/controller/ScheduleControllerTest.java
+++ src/test/java/ua/com/foxminded/university/controller/ScheduleControllerTest.java
@@ -1,19 +1,74 @@
 package ua.com.foxminded.university.controller;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import ua.com.foxminded.university.model.lesson.Specialism;
+import ua.com.foxminded.university.model.schedule.AcademicYear;
+import ua.com.foxminded.university.model.schedule.SemesterType;
+import ua.com.foxminded.university.model.user.Group;
+import ua.com.foxminded.university.model.view.GroupView;
+import ua.com.foxminded.university.service.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = {ScheduleController.class})
 public class ScheduleControllerTest {
     @Autowired
-    private ScheduleController scheduleController;
+    private MockMvc mockMvc;
+
+    @MockBean
+    LessonService lessonService;
+
+    @MockBean
+    DisciplineService disciplineService;
+    @MockBean
+    GroupService groupService;
+    @MockBean
+    RoomService roomService;
+    @MockBean
+    ScheduleDayService scheduleDayService;
+
+    @MockBean
+    SpecialismService specialismService;
+    @MockBean
+    EducatorService educatorService;
+    @MockBean
+    AcademicYearService academicYearService;
 
     @Test
-    public void shouldClarifyProblemWithConstraint() {
+    public void shouldClarifyProblemWithConstraint() throws Exception {
+        Specialism specialism = new Specialism(1L, "Specialism");
+        AcademicYear academicYear = new AcademicYear(1L, 1, SemesterType.SPRING_SEMESTER);
+        String groupName = "XX-01";
+        when(groupService.findAll()).thenReturn(List.of(
+                new Group(1L,
+                        groupName,
+                        specialism,
+                        academicYear)));
+        when(specialismService.findById(1L)).thenReturn(specialism); // do we need additional db trip instead group.getSpecialism() ?
+        when(academicYearService.findById(1L)).thenReturn(academicYear); // same as above
 
+        List<GroupView> expected = List.of(
+                new GroupView(
+                        1L,
+                        groupName,
+                        specialism.getSpecialismName(),
+                        academicYear.getYearNumber(),
+                        academicYear.getSemesterType()
+                )
+        );
+
+        mockMvc.perform(get("/schedule/list/groups")
+                        .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("groups", expected));
     }
 }
